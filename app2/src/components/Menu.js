073684b4b500883ae0272ef1ae9f5a88d463f758
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2022-11-21 17:35:51 +0530

    React Router completed...

--- app2/src/components/Menu.js
+++ app2/src/components/Menu.js
@@ -1,13 +1,15 @@
+
 import React from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
+import {Link} from "react-router-dom";
 
 function Menu() {
     return (
         <>
             <ListGroup>
-                <ListGroupItem tag="a" href="/" action>Home</ListGroupItem>
-                <ListGroupItem tag="a" href="/add-courses" action>Add Courses</ListGroupItem>
-                <ListGroupItem tag="a" href="/all-courses" action>All Courses</ListGroupItem>
+                <Link className="list-group-item list-group-item-action" tag="a" to="/" action>Home</Link>
+                <Link className="list-group-item list-group-item-action" tag="a" to="/add-courses" action>Add Courses</Link>
+                <Link className="list-group-item list-group-item-action" tag="a" to="/all-courses" action>All Courses</Link>
             </ListGroup>
         </>
     );

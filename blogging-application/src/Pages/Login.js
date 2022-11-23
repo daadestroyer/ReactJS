974c374b87234451c255280d7ba4845e2a1405fc
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2022-11-23 12:26:55 +0530

    login and signup form created...

--- blogging-application/src/Pages/Login.js
+++ blogging-application/src/Pages/Login.js
@@ -1,15 +1,43 @@
 import React from 'react'
+import { Button, Card, CardBody, CardFooter, CardHeader, Col, Container, Form, FormGroup, Input, Row } from 'reactstrap'
 import Base from '../Components/Base'
 
 const Login = () => {
   return (
-    <div className='text-center'>
+    <div >
       <Base>
-        <h1>This is login page</h1>
-        <p>Welcome to login page</p>
+        <Container className='mt-5'>
+          <Row>
+             
+            <Col md={{size:6,offset:3}}>
+              <Card color='dark' outline>
+                <CardHeader className='bg-dark text-white'>Login Page</CardHeader>
+                <CardBody>
+                  <Form>
+                    <FormGroup>
+                      <label>Email</label>
+                      <Input type='email' name='uname' placeholder='Enter your username' />
+                    </FormGroup>
+                    <FormGroup>
+                      <label>Password</label>
+                      <Input type='password' name='upwd' placeholder='Enter your password' />
+                    </FormGroup>
+                    <Container className='text-center'>
+                      <Button outline color='success'>Login</Button>{" "}
+                      <Button type='reset'  outline color='warning'>Reset</Button>
+                    </Container>
+                  </Form>
+                </CardBody>
+                <CardFooter className='bg-dark text-danger'>Here login error will come</CardFooter>
+              </Card>
+            </Col>
+            
+          </Row>
+        </Container>
       </Base>
     </div>
   )
 }
 
 export default Login
+
--- blogging-application/src/Pages/Signup.js
+++ blogging-application/src/Pages/Signup.js
@@ -1,12 +1,47 @@
 import React from 'react'
+import { Button, Card, CardBody, CardFooter, CardHeader, Col, Container, Form, FormGroup, Input, Row } from 'reactstrap'
 import Base from '../Components/Base'
 
 const Signup = () => {
   return (
-    <div className='text-center'>
+    <div>
       <Base>
-        <h1>This is signup page</h1>
-        <p>Welcome to signup page</p>
+        <Container className='mt-5'>
+          <Row>
+             
+            <Col md={{size:6,offset:3}}>
+              <Card color='dark' outline>
+                <CardHeader className='bg-dark text-white'>Signup Page</CardHeader>
+                <CardBody>
+                  <Form>
+                    <label>Name</label>
+                    <FormGroup>
+                      <Input type='text' name='uname' placeholder='Enter your name' />
+                    </FormGroup>
+                    <label>Email</label>
+                    <FormGroup>
+                      <Input type='email' name='uemail' placeholder='Enter your email' />
+                    </FormGroup>
+                    <label>Password</label>
+                    <FormGroup>
+                      <Input type='password' name='upwd' placeholder='Enter your password' />
+                    </FormGroup>
+                    <label>About</label>
+                    <FormGroup>
+                      <Input type='textarea' name='uabout' placeholder='Enter about you' style={{ height: 100 }} />
+                    </FormGroup>
+                    <Container className='text-center'>
+                      <Button outline color='success'>Signup</Button>{" "}
+                      <Button type='reset' outline color='warning'>Reset</Button>
+                    </Container>
+                  </Form>
+                </CardBody>
+                <CardFooter className='bg-dark text-danger'>Here login error will come</CardFooter>
+              </Card>
+            </Col>
+             
+          </Row>
+        </Container>
       </Base>
     </div>
   )
